4f05f5ddc7fffcc0be52c525cbffbb0c73e6e266
Sophpaige <159298204+Sophpaige@users.noreply.github.com>
2024-12-22 12:36:17 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-march-sophie1"
+    bucket         = "hellosophie"
     region         = "eu-west-2"
     key            = "Github-Runner-TF/terraform.tfstate"
     encrypt        = true
