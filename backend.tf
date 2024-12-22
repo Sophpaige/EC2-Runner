7d952594333b53d5be6d5591b5a0693bd21e05cf
Sophpaige <159298204+Sophpaige@users.noreply.github.com>
2024-12-22 12:32:07 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = ****CHANGEBUCKETNAME***
+    bucket         = "terraform-march-sophie1"
     region         = "eu-west-2"
     key            = "Github-Runner-TF/terraform.tfstate"
     encrypt        = true
